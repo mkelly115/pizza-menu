ff2c876a3b923875782781f1f7a3e7422946b646
Michael <mikek222@gmail.com>
2025-06-04 11:58:26 -0400

    testing JS within react

--- src/index.js
+++ src/index.js
@@ -71,6 +71,14 @@ function Menu() {
 }
 
 function Footer() {
+
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+
+  if( hour >= openHour && hour <= closeHour) alert("We're Currently Open!")
+  else alert("Sorry we're Closed");
+
   return (
     <footer>{new Date().toLocaleTimeString()}. We're currently open!</footer>
   );
