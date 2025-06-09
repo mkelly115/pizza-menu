4def1d4cbf78dc7ec14555d5850c42d362a9e055
Michael <mikek222@gmail.com>
2025-06-09 19:37:24 -0400

    added operators to conditionally render pizza

--- src/index.js
+++ src/index.js
@@ -70,20 +70,21 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length
+
   return (
     <li className="menu">
       <h2>Our Menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => {
-          return (
-            <Pizza
-            pizzaObj={pizza}
-            key={pizza.name}
-            />
-          );
-        })}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => {
+            return <Pizza pizzaObj={pizza} key={pizza.name} />;
+          })}
+        </ul>
+      )}
+
       {/* <Pizza
         name="Pizza Spinnaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
@@ -112,7 +113,12 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're currently open!
+      {isOpen && (
+        <div className="order">
+          <p>We're Open until {closeHour}:00. Come visit us or order online</p>
+          <button className="btn">order</button>
+        </div>
+      )}
     </footer>
   );
 }
